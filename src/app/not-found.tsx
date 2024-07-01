842fececcd2505a1591eb5d16d123d82140dba3d
hwanheejung <hwanheejung117@gmail.com>
2024-07-01 14:43:47 +0900

    #5 feat: not-found 페이지 추가

---
+++ src/app/not-found.tsx
@@ -0,0 +1,9 @@
+const NotFoundPage = () => {
+  return (
+    <main className="min-h-screen flex justify-center items-center">
+      <h1>존재하지 않는 페이지입니다</h1>
+    </main>
+  )
+}
+
+export default NotFoundPage
