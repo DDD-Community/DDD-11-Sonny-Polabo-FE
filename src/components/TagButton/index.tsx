2dd01df481913f42aa50fc354ba590fc34200b11
junseublim <junslim11@gmail.com>
2024-07-09 01:10:23 +0900

    Feat: Add Tag Button

---
+++ src/components/TagButton/index.tsx
@@ -0,0 +1,51 @@
+import React from 'react'
+import { twMerge } from 'tailwind-merge'
+
+interface TagButtonProps extends React.ButtonHTMLAttributes<HTMLButtonElement> {
+  size: 'small' | 'medium'
+  className?: string
+  isSubmit?: boolean
+}
+
+function TagButton({
+  className,
+  children,
+  isSubmit,
+  disabled,
+  size,
+  ...props
+}: TagButtonProps) {
+  const getSizeClass = () => {
+    if (size === 'small') {
+      return 'text-sm rounded-[36px] px-3 h-6.5'
+    }
+    if (size === 'medium') {
+      return 'text-md rounded-[36px] px-5 h-8'
+    }
+    return ''
+  }
+
+  const buttonClass = twMerge(
+    'whitespace-normal break-words border border-gray-900 bg-gray-0 active:bg-gray-100',
+    getSizeClass(),
+    className,
+  )
+
+  return (
+    <button
+      className={buttonClass}
+      type={isSubmit ? 'submit' : 'button'}
+      {...props}
+      disabled={disabled}
+    >
+      {children}
+    </button>
+  )
+}
+
+TagButton.defaultProps = {
+  className: '',
+  isSubmit: false,
+}
+
+export default TagButton
