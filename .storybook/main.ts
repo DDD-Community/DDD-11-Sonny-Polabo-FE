110b91039b6aba689afb99d3fdc1207f4ae8cf8f
junseublim <junslim11@gmail.com>
2024-07-11 02:03:44 +0900

    Feat: Add Modal Component

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -14,5 +14,23 @@ const config: StorybookConfig = {
     options: {},
   },
   staticDirs: ['../public'],
+  webpackFinal: async (config) => {
+    config.module = config.module || {}
+    config.module.rules = config.module.rules || []
+    const imageRule = config.module.rules.find((rule) =>
+      // @ts-ignore
+      rule?.['test']?.test('.svg'),
+    )
+    if (imageRule) {
+      // @ts-ignore
+      imageRule['exclude'] = /\.svg$/
+    }
+    config.module.rules.push({
+      test: /\.svg$/,
+      use: ['@svgr/webpack'],
+    })
+
+    return config
+  },
 }
 export default config
