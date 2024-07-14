4487f7f80bd7382e4342dda8978b8d0783eb1553
junseublim <junslim11@gmail.com>
2024-07-14 14:29:07 +0900

    #32 Feat: Markup for home page

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -1,13 +1,35 @@
-import Link from 'next/link'
+import PolaroidsIcon from 'public/icons/home_polaroids.svg'
+import LinkIcon from 'public/icons/linkcopy.svg'
+import PolaboLogo from 'public/images/polabo_logo.png'
+import Image from 'next/image'
+import Button from '@/components/Button'
 
 const HomePage = () => {
   return (
-    <div className="flex flex-col items-center justify-between p-24">
-      <div className="flex flex-col items-center">
-        <h2>함께 꾸미는 폴라로이드 보드</h2>
-        <h1 className="text-4xl font-bold">POLABO</h1>
+    <div className="h-dvh flex flex-col items-center justify-between">
+      <div className="pt-12">
+        <PolaroidsIcon className="m-auto" />
+        <span className="pt-2 font-jooree text-sm leading-4 block text-center m-auto">
+          함께 꾸미는 폴라로이드 보드
+        </span>
+        <Image src={PolaboLogo} alt="logo" className="px-20 py-0.5" />
       </div>
-      <Link href="/board/create">시작하기</Link>
+      <div className="flex flex-col items-center mb-[30px]">
+        <div className="text-center mb-2.5">지금까지 몇명이 공유했어요!</div>
+        <Button size="lg" className="mb-3">
+          시작하기
+        </Button>
+        <div className="text-center text-gray-400 text-xxs leading-3 mb-1">
+          copy link!
+        </div>
+        <button
+          type="button"
+          className="p-3 bg-gray-200 rounded-[30px]"
+          aria-label="copy link"
+        >
+          <LinkIcon />
+        </button>
+      </div>
     </div>
   )
 }
