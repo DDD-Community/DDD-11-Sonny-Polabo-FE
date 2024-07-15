6f014872ce4111f9ef4e8bc377987877dc2ca2f3
junseublim <junslim11@gmail.com>
2024-07-15 16:16:49 +0900

    #33 Feat: disable button on invalid input

--- src/app/(board)/board/create/page.tsx
+++ src/app/(board)/board/create/page.tsx
@@ -5,15 +5,12 @@ import BoardNameRecommendations from './components/BoardNameRecommendations'
 
 const CreateBoardPage = () => {
   return (
-    <div className="flex flex-col items-center">
+    <div className="flex flex-col h-dvh items-center justify-between">
       <Image
         src={PolaboLogo}
         alt="logo"
         className="px-20 pt-6 object-contain"
       />
-      <div className="text-gray-900 text-lg font-thin leading-6 py-9">
-        보드 주제를 정해주세요!
-      </div>
       <BoardNameForm>
         <BoardNameRecommendations />
       </BoardNameForm>
