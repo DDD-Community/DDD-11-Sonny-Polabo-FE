6c419015cc7cf50cff674cae050b746f9ddc478b
junseublim <junslim11@gmail.com>
2024-07-18 23:10:43 +0900

    feat: Set revalidate time to 1 hour for total board counts

--- src/lib/api/board.ts
+++ src/lib/api/board.ts
@@ -24,7 +24,7 @@ export const postBoard = async (
 export const getTotalBoards = async (): Promise<number> => {
   const res = await get('/api/v1/boards/total-count', {
     next: {
-      tags: ['totalBoards'],
+      revalidate: 60 * 60, // 1 hour
     },
   })
 
