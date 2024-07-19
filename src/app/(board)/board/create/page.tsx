103b2ae60ab74e41081d1444655b87c9e673fd0c
junseublim <junslim11@gmail.com>
2024-07-20 01:53:59 +0900

    #44 feat: Add BoardAvailabilityCheckModal

--- src/app/(board)/board/create/page.tsx
+++ src/app/(board)/board/create/page.tsx
@@ -2,6 +2,7 @@ import PolaboLogo from 'public/images/polabo_logo.png'
 import Image from 'next/image'
 import BoardNameForm from './components/BoardNameForm'
 import BoardNameRecommendations from './components/BoardNameRecommendations'
+import BoardAvailabilityCheckModal from './components/BoardAvailabilityCheckModal'
 
 const CreateBoardPage = () => {
   return (
@@ -11,6 +12,7 @@ const CreateBoardPage = () => {
         alt="logo"
         className="px-20 pt-6 object-contain"
       />
+      <BoardAvailabilityCheckModal />
       <BoardNameForm>
         <BoardNameRecommendations />
       </BoardNameForm>
