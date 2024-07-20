bce824c54758754dbb4e6e8337b6df4e457863d9
hwanheejung <hwanheejung117@gmail.com>
2024-07-20 16:43:55 +0900

    #46 style: board share modal close icon 추가

--- src/app/(board)/board/[boardId]/components/ShareBtn.tsx
+++ src/app/(board)/board/[boardId]/components/ShareBtn.tsx
@@ -1,10 +1,10 @@
 'use client'
 
 import Modal from '@/components/Modal'
+import CopyIcon from 'public/icons/copy.svg'
 import Share from 'public/icons/ios_share.svg'
 import TwoPolaroidsIcon from 'public/icons/twopolaroids.svg'
 import { useEffect, useState } from 'react'
-import CopyIcon from 'public/icons/copy.svg'
 
 const ShareBtn = () => {
   const [showShareModal, setShowShareModal] = useState<boolean>(false)
@@ -21,26 +21,26 @@ const ShareBtn = () => {
   return (
     <>
       <Share onClick={() => setShowShareModal(true)} className="w-6" />
-      {showShareModal && (
-        <Modal isOpen={showShareModal} onClose={() => setShowShareModal(false)}>
-          <Modal.CenterModal icon={<TwoPolaroidsIcon />}>
-            <Modal.Title>보드를 친구에게 공유해보세요!</Modal.Title>
-            <Modal.Content>
-              <div className="truncate">{currentURL}</div>
-            </Modal.Content>
 
-            <Modal.CenterConfirm
-              confirmText={
-                <>
-                  <CopyIcon />
-                  복사하기
-                </>
-              }
-              onConfirm={copyLink}
-            />
-          </Modal.CenterModal>
-        </Modal>
-      )}
+      <Modal isOpen={showShareModal} onClose={() => setShowShareModal(false)}>
+        <Modal.CenterModal icon={<TwoPolaroidsIcon />}>
+          <Modal.Close />
+          <Modal.Title>보드를 친구에게 공유해보세요!</Modal.Title>
+          <Modal.Content>
+            <div className="truncate">{currentURL}</div>
+          </Modal.Content>
+
+          <Modal.CenterConfirm
+            confirmText={
+              <>
+                <CopyIcon />
+                복사하기
+              </>
+            }
+            onConfirm={copyLink}
+          />
+        </Modal.CenterModal>
+      </Modal>
     </>
   )
 }
