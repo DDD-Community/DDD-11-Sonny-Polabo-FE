95e5ca1563226ee4d7807222d20d9d189358aae0
junseublim <junslim11@gmail.com>
2024-07-20 17:11:44 +0900

    feat: Add favicon

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -30,6 +30,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <head>
+        <link rel="icon" href="/images/favicon.ico" sizes="any" />
+      </head>
       <body
         className={`${PretendVariable.variable} ${Jooree.variable} ${Hesom.variable}`}
       >
