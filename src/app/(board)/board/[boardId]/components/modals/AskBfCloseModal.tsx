650d3164a0114a4f918385044a19d4303b102e8d
hwanheejung <hwanheejung117@gmail.com>
2024-07-23 21:34:07 +0900

    #51 fix: modal 수정 rollback

--- src/app/(board)/board/[boardId]/components/modals/AskBfCloseModal.tsx
+++ src/app/(board)/board/[boardId]/components/modals/AskBfCloseModal.tsx
@@ -1,4 +1,3 @@
-import Button from '@/components/Button'
 import Modal from '@/components/Modal'
 import SurprisedIcon from 'public/icons/surprised.svg'
 
@@ -9,33 +8,16 @@ interface ModalProps {
 }
 
 const AskBfCloseModal = ({ isOpen, onClose, onConfirm }: ModalProps) => {
-  const clickHandler = () => {
-    return onClose().then(() => onConfirm())
-  }
-
   return (
     <Modal isOpen={isOpen} onClose={onClose}>
       <Modal.CenterModal icon={<SurprisedIcon />}>
         <Modal.Title>폴라로이드 등록을 중단할까요?</Modal.Title>
         <Modal.Content>여태까지 작성한 내용이 사라져요.</Modal.Content>
-        <div className="my-4 flex gap-1.5">
-          <Button
-            variant="secondary"
-            size="md"
-            className="w-[135px]"
-            onClick={clickHandler}
-          >
-            예
-          </Button>
-          <Button
-            variant="primary"
-            size="md"
-            className="w-[135px]"
-            onClick={onClose}
-          >
-            아니오
-          </Button>
-        </div>
+        <Modal.BottomConfirmCancel
+          cancelText="아니요"
+          confirmText="예"
+          onConfirm={onConfirm}
+        />
       </Modal.CenterModal>
     </Modal>
   )
