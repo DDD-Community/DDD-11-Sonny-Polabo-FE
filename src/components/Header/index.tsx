7fb9f38bb0392530b5c23f22c337b5a8f53e9808
junseublim <junslim11@gmail.com>
2024-08-06 22:43:40 +0900

    #67 Feat: header style - title optional - grid - shadow-header

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -2,29 +2,31 @@ import { ReactNode } from 'react'
 import HeaderBackButton from '@/components/Header/HeaderBackButton'
 
 interface HeaderProps {
-  title: string
+  title?: string
   description?: string
   leftButton?: ReactNode
   rightButton?: ReactNode
 }
 
 const Header = ({
-  title,
+  title = '',
   description = '',
-  leftButton = <div />,
-  rightButton = <div />,
+  leftButton = null,
+  rightButton = null,
 }: HeaderProps) => {
   return (
     <>
-      <header className="fixed flex h-16 w-full max-w-md justify-between bg-gray-0 p-5 shadow-[0_1px_2px_0_rgba(0,0,0,0.10)]">
-        {leftButton}
-        <div className="flex flex-col items-center">
-          <div className="text-md font-semiBold leading-6">{title}</div>
-          <div className="mt-1 text-xs font-regular leading-4 text-gray-700">
+      <header className="fixed grid h-16 w-full max-w-md grid-cols-3 justify-between bg-gray-0 p-5 shadow-header">
+        <div className="justify-self-start">{leftButton}</div>
+        <div className="justify-self-center">
+          <div className="text-center text-md font-semiBold leading-6">
+            {title}
+          </div>
+          <div className="mt-1 text-center text-xs font-regular leading-4 text-gray-700">
             {description}
           </div>
         </div>
-        {rightButton}
+        <div className="justify-self-end">{rightButton}</div>
       </header>
       <div className="block h-16 w-full" />
     </>
