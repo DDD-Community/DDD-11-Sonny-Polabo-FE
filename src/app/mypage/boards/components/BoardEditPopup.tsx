f30697b6f92fbd751841a52a8af3f2d524970f53
junseublim <junslim11@gmail.com>
2024-08-21 22:36:34 +0900

    feat: 보드 삭제 아이콘 추가

--- src/app/mypage/boards/components/BoardEditPopup.tsx
+++ src/app/mypage/boards/components/BoardEditPopup.tsx
@@ -1,5 +1,6 @@
 import Popup from '@/components/Popup'
 import React from 'react'
+import DeleteIcon from 'public/icons/delete.svg'
 
 interface BoardEditPopupProps {
   isOpen: boolean
@@ -14,9 +15,13 @@ const BoardEditPopup = ({
 }: BoardEditPopupProps) => {
   return (
     <Popup isOpen={isOpen} close={close}>
-      <div className="flex w-[168px] cursor-pointer flex-col rounded-lg bg-gray-0 py-2 pl-[22px] shadow-popup">
-        <div className="text-sm text-negative" onClick={clickDelete}>
+      <div className="flex w-[168px] cursor-pointer flex-col rounded-lg bg-gray-0 py-2 pl-[22px] pr-[12px] shadow-popup">
+        <div
+          className="flex items-center justify-between text-sm text-negative"
+          onClick={clickDelete}
+        >
           <span>보드 삭제하기</span>
+          <DeleteIcon />
         </div>
       </div>
     </Popup>
