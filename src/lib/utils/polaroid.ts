cd9e896dd1ac9d4d3249cfda67d28e79079d1726
junseublim <junslim11@gmail.com>
2024-09-03 20:00:10 +0900

    feat: getPolaroidNickname 로직 수정

--- src/lib/utils/polaroid.ts
+++ src/lib/utils/polaroid.ts
@@ -3,14 +3,4 @@ import { Session } from 'next-auth'
 export const getPolaroidNickname = (
   nickname: string,
   session: Session | null,
-) => {
-  if (nickname !== '') {
-    return nickname
-  }
-
-  if (session?.profile?.nickName) {
-    return nickname
-  }
-
-  return '익명'
-}
+) => nickname || session?.profile?.nickName || '익명'
