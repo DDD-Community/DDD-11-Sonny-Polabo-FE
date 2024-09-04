07d7cce9f43db2b7ca1267cc736d7e648518ac77
hwanheejung <hwanheejung117@gmail.com>
2024-09-04 14:35:29 +0900

    #103 fix: 초기화 제거

--- src/app/mypage/boards/_components/BoardList/index.tsx
+++ src/app/mypage/boards/_components/BoardList/index.tsx
@@ -24,10 +24,6 @@ const BoardList = () => {
   const fetchBoards = async (page = 1, size = 10) => {
     const filter = isParticipant ? 'PARTICIPANT' : 'OWNER'
 
-    // 초기화
-    setBoards([])
-    setPagination({ totalPage: 0, totalCount: 0, currentPage: 1, size: 10 })
-
     return getMyBoards(page, size, filter).then((data) => {
       setBoards(data.boards)
       setPagination(data.pagination)
@@ -37,6 +33,10 @@ const BoardList = () => {
   useEffect(() => {
     fetchBoards()
   }, [searchParams])
+
+  useEffect(() => {
+    console.log(boards)
+  }, [boards])
 
   const paginate = async (page: number) => {
     return fetchBoards(page, pagination.size)
