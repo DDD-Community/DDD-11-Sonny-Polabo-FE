aeaf6567b176a3cd65876f97d44a759ad88cf7ee
hwanheejung <hwanheejung117@gmail.com>
2024-09-12 13:14:34 +0900

    #120 feat: import image dynamically

--- src/app/board/[boardId]/_components/StickerModal/index.tsx
+++ src/app/board/[boardId]/_components/StickerModal/index.tsx
@@ -1,19 +1,40 @@
 'use client'
 
-import Menu0 from 'public/stickers/0.svg'
-import Menu1 from 'public/stickers/1.svg'
+import Image from 'next/image'
+import { StickerMenu } from '@/types'
 import Header from './Header'
 import Menu from './Menu'
 import { StickerProvider } from './StickerContext'
 
+interface StickerIconProps {
+  num: StickerMenu
+}
+
+const StickerIcon = ({ num }: StickerIconProps) => {
+  return (
+    <Image
+      src={`/stickers/${num}.svg`}
+      width={34}
+      height={34}
+      alt="sticker menu"
+    />
+  )
+}
+
 const CreateSticker = () => {
+  const stickers: StickerMenu[] = [0, 1, 2, 3]
   return (
     <div className="w-md mx-auto flex h-dvh max-w-md flex-1 flex-col bg-gray-1000/70 py-10 backdrop-blur-md">
       <Header />
       <StickerProvider>
         <Menu>
-          <Menu.Item icon={<Menu0 />} menuNum={0} />
-          <Menu.Item icon={<Menu1 />} menuNum={1} />
+          {stickers.map((sticker) => (
+            <Menu.Item
+              icon={<StickerIcon num={sticker} />}
+              menuNum={sticker}
+              key={sticker}
+            />
+          ))}
         </Menu>
       </StickerProvider>
     </div>
