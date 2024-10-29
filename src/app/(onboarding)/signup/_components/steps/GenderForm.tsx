4db5b6aa359a08a57cfd25c547b223630ef6487d
junseublim <junslim11@gmail.com>
2024-10-29 23:51:35 +0900

    feat: 보드 주제 -> 이름 변경

--- src/app/(onboarding)/signup/_components/steps/GenderForm.tsx
+++ src/app/(onboarding)/signup/_components/steps/GenderForm.tsx
@@ -37,7 +37,7 @@ const GenderForm = ({
         {'을 \n 입력해주세요!'}
       </div>
       <p className="text-sm text-gray-400">
-        추가 정보를 입력하시면 나에게 딱 맞는 보드 주제를 추천해드려요 :)
+        추가 정보를 입력하시면 나에게 딱 맞는 보드 이름을 추천해드려요 :)
       </p>
 
       <div className="mx-auto mb-6 flex flex-1 items-center gap-2">
