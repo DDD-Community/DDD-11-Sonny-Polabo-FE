3a4373cfed9adfd643422a4fa07ac661ec773db1
junseublim <junslim11@gmail.com>
2024-11-10 16:23:24 +0900

    플라로이드 메이커 PolaroidMessage 수정

--- src/components/Polaroid/PolaroidMaker/PolaroidMessageInput.tsx
+++ src/components/Polaroid/PolaroidMaker/PolaroidMessageInput.tsx
@@ -1,9 +1,10 @@
 import { preventKeyboardSubmit } from '@/lib/utils/keyboard'
+import { useEffect, useRef } from 'react'
 
 interface PolaroidMessageInputProps {
   message: string
   maxLength: number
-  changeMessage: (e: React.ChangeEvent<HTMLInputElement>) => void
+  changeMessage: (e: React.ChangeEvent<HTMLTextAreaElement>) => void
 }
 
 const PolaroidMessageInput = ({
@@ -11,17 +12,29 @@ const PolaroidMessageInput = ({
   maxLength,
   changeMessage,
 }: PolaroidMessageInputProps) => {
+  const ref = useRef<HTMLTextAreaElement>(null)
+
+  useEffect(() => {
+    if (!ref.current) {
+      return
+    }
+
+    ref.current.style.height = 'auto' // 높이를 초기화하여 기존 높이를 제거
+    ref.current.style.height = `${ref.current.scrollHeight}px`
+  }, [message])
+
   return (
-    <div className="text-lg tracking-tight">
-      <input
-        type="text"
+    <div className="tracking-tight">
+      <textarea
+        ref={ref}
         value={message}
         onChange={changeMessage}
         onKeyDown={preventKeyboardSubmit}
-        className="h-[25px] w-full bg-transparent outline-none"
+        className="w-full resize-none bg-transparent text-xl leading-6 outline-none"
         maxLength={maxLength}
         placeholder="눌러서 한줄 문구를 입력하세요"
         name="oneLineMessage"
+        rows={1}
       />
       <p className="text-right text-sm text-gray-400">
         {message.length}/{maxLength}자
