7c917efdb3eb06a1e5676793ddc2e21c6dac0d98
hwanheejung <hwanheejung117@gmail.com>
2024-12-12 00:00:35 +0900

    feat: adapt board header for dark theme

--- src/lib/constants/boardConfig.ts
+++ src/lib/constants/boardConfig.ts
@@ -1,20 +1,28 @@
-export const BOARDTHEMAS = {
+import { BoardThemaKeyType, BoardThemaType } from '@/types'
+
+export const BOARDTHEMAS: Record<BoardThemaKeyType, BoardThemaType> = {
   'B-0': {
     title: '기본 테마',
+    theme: 'LIGHT',
   },
   'B-1': {
     title: '밝은 테마',
+    theme: 'LIGHT',
   },
   'B-2': {
     title: '수능 테마',
+    theme: 'LIGHT',
   },
   'B-3': {
     title: '코르크판 테마',
+    theme: 'LIGHT',
   },
   'B-4': {
     title: '크리스마스 테마',
+    theme: 'DARK',
   },
   'B-5': {
     title: '크리스마스 트리 테마',
+    theme: 'DARK',
   },
 }
