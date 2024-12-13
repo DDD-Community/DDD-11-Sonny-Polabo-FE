479ae51fac8b68382b3ec87a717d981491cc2264
hwanheejung <hwanheejung117@gmail.com>
2024-12-13 13:37:03 +0900

    feat: add Google Tag Manager

--- .env.d.ts
+++ .env.d.ts
@@ -2,5 +2,7 @@ declare namespace NodeJS {
   interface ProcessEnv {
     API_HOST: string
     NEXT_PUBLIC_KAKAO_API_KEY: string
+    NEXT_PUBLIC_GA_ID: string
+    NEXT_PUBLIC_GTM_ID: string
   }
 }
