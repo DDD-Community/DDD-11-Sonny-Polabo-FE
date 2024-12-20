769dc822d6419225056d79d736229cb9ca72fe4f
hwanheejung <hwanheejung117@gmail.com>
2024-12-20 18:47:47 +0900

    feat: show tutorial to new user

--- src/components/Tutorial/CheckNewUser.tsx
+++ src/components/Tutorial/CheckNewUser.tsx
@@ -7,19 +7,17 @@ const CheckNewUser = () => {
   const { data: session } = useSession()
 
   useEffect(() => {
-    localStorage.setItem('needBoardTutorial', 'true')
-    localStorage.setItem('needDecorateTutorial', 'true')
-    // if (localStorage.getItem('needBoardTutorial') === null) {
-    //   if (!session || session?.newUser) {
-    //     // 비회원이거나 신규 회원
-    //     localStorage.setItem('needBoardTutorial', 'true')
-    //     localStorage.setItem('needDecorateTutorial', 'true')
-    //   } else {
-    //     // 기존 회원
-    //     localStorage.setItem('needBoardTutorial', 'false')
-    //     localStorage.setItem('needDecorateTutorial', 'false')
-    //   }
-    // }
+    if (localStorage.getItem('needBoardTutorial') === null) {
+      if (!session || session?.newUser) {
+        // 비회원이거나 신규 회원
+        localStorage.setItem('needBoardTutorial', 'true')
+        localStorage.setItem('needDecorateTutorial', 'true')
+      } else {
+        // 기존 회원
+        localStorage.setItem('needBoardTutorial', 'false')
+        localStorage.setItem('needDecorateTutorial', 'false')
+      }
+    }
   }, [session])
 
   return null
